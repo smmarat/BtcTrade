d8d9528d069ae3059fe41518eda97be8fde1f788
Marat Shmush <smmarat@gmail.com>
2017-02-10 16:04:52 +0200

    fabric added

--- app/src/main/java/multico/in/btctrade/App.java
+++ app/src/main/java/multico/in/btctrade/App.java
@@ -3,6 +3,8 @@ package multico.in.btctrade;
 import android.app.Application;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by Smmarat on 16.01.17.
@@ -22,6 +24,7 @@ public class App extends Application {
         instance = this;
         pref = PreferenceManager.getDefaultSharedPreferences(this);
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
     }
 
 }
